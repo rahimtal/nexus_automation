089b6f8b88cdb77939eddcccb9c8079a48c67bc2
talharahim <49953963+talharahim@users.noreply.github.com>
2024-08-27 05:56:16 -0700

    ResponseVersion

--- src/test/java/Public/portalconsumptionControllerV4.java
+++ src/test/java/Public/portalconsumptionControllerV4.java
@@ -13,27 +13,37 @@ import io.restassured.response.ValidatableResponse;
 
 public class portalconsumptionControllerV4 {
 
-	public static ValidatableResponse jsonPathEvaluator;
-
-	@Test(priority = 1, groups = "portalConsumptionHistory")
-	public void getconsumptionHistoryController() throws ClassNotFoundException, SQLException, InterruptedException, IOException {
+	@Test(priority = 1, groups = "portalConsumption")
+	public void getconsumptionHistoryController()
+			throws ClassNotFoundException, SQLException, InterruptedException, IOException {
 
 		String uri = "/portal/consumptionHistory";
 		String ver = "4.0";
 		String jpath = "./\\TestData\\portalconsumptionHistory_v4.json";
-				
+
 		HashMap<String, String> params = new HashMap<String, String>();
 		params.put("LocationId", "METERGRP01");
 		params.put("CustomerId", "500024119");
 		params.put("UserDate", "2025-01-01");
-				
+
 		String result = CommonMethods.getMethod(uri, ver, params, jpath);
 		System.out.println(result);
-		
+
 	}
-	
-	
-		
-	
+
+	@Test(priority = 2, groups = "portalConsumption")
+	public void getserviceByLocation() throws ClassNotFoundException, SQLException, InterruptedException, IOException {
+
+		String uri = "/portal/serviceByLocation";
+		String ver = "4.0";
+		String jpath = "./\\TestData\\portalserviceByLocation.json";
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("LocationId", "METERGRP01");
+		params.put("CustomerId", "500024119");
+		params.put("ResponseVersion", "2");
+		String result = CommonMethods.getMethod(uri, ver, params, jpath);
+		System.out.println(result);
+
+	}
 
 }
