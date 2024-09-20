9cf3b03b08fb674c7f82dab951f1bbf661baa6f1
talharahim <49953963+talharahim@users.noreply.github.com>
2024-09-20 00:14:42 -0700

    update

--- src/main/java/com/NexustAPIAutomation/java/verifyPDFReports.java
+++ src/main/java/com/NexustAPIAutomation/java/verifyPDFReports.java
@@ -44,4 +44,35 @@ public class verifyPDFReports {
 		// Disconnect the HttpURLConnection stream
 		connection.disconnect();
 	}
+
+	public static String returnPDF(String uri) throws Exception {
+		// URL of the REST API that returns a PDF
+		URL url = new URL("http://localhost:3000/api/v4" + uri);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		String basicAuth = "Bearer " + CommonMethods.getToken();
+		// Set the request method (GET is default)
+		connection.setRequestProperty("Authorization", basicAuth);
+		connection.setRequestMethod("GET");
+		// Get the response code
+		int responseCode = connection.getResponseCode();
+		// If successful (HTTP_OK = 200), read the input stream and save it
+		if (responseCode == HttpURLConnection.HTTP_OK) {
+			try (InputStream is = connection.getInputStream()) {
+				// Load the PDF document
+				PDDocument document = PDDocument.load(is);
+				// Instantiate PDFTextStripper class
+				PDFTextStripper pdfStripper = new PDFTextStripper();
+				// Retrieving text from PDF document
+				String text = pdfStripper.getText(document);
+				// Check if the PDF contains the expected text
+				document.close();
+				return text;
+			}
+		} else {
+			System.out.println("GET request not worked");
+		}
+		// Disconnect the HttpURLConnection stream
+		connection.disconnect();
+		return null;
+	}
 }
