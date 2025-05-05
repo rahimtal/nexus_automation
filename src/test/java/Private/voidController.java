8dc3d64eb43a367e239c4a8ac62b58e156c8aa10
talharahim <49953963+talharahim@users.noreply.github.com>
2025-05-05 18:44:06 +0500

    update

--- src/test/java/Private/voidController.java
+++ src/test/java/Private/voidController.java
@@ -42,7 +42,7 @@ public class voidController {
 
 	@Test(priority = 1, groups = "void")
 	public void putvoidv4() throws ClassNotFoundException, SQLException, InterruptedException, IOException {
-		CommonMethods.Bug("https://cogsdale.atlassian.net/browse/CPDEV-17270");
+		//CommonMethods.Bug("https://cogsdale.atlassian.net/browse/CPDEV-17270");
 		String uri = "/void";
 		String ver = "4.0";
 		String jpath = "./\\TestData\\putvoidv4.json";
