c42e3632a3721dabba718cb50ac3abeba8323672
talharahim <talharahim@gmail.com>
2026-01-13 14:22:03 +0500

    MS

--- src/test/java/com/NexusAPI/tests/BaseClass.java
+++ src/test/java/com/NexusAPI/tests/BaseClass.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import com.NexustAPIAutomation.java.QuickDBRestore;
+
 //import com.aventstack.extentreports.ExtentReports;
 
 //import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
@@ -14,26 +16,25 @@ public class BaseClass {
 	// public static ExtentReports extent;
 	// public static //ExtentTest test;
 	// public static ExtentHtmlReporter htmlReporter;
-	
+
 	@BeforeSuite
 	public void setupReport() throws IOException {
 		System.out.println("Deleting folder");
 		Runtime.getRuntime().exec("cmd /c rd /s /q C:\\Users\\Admin\\Documents\\GitHub\\nexus_automation\\test-output");
-		}
+		QuickDBRestore.restoreDatabase();
 
-	 @AfterSuite
-	    public void sendReportByEmail() throws Exception {
-	        // Delay added just to be safe
-	        Thread.sleep(3000);
+	}
 
-	        String from = "cogsauto@gmail.com";
-	        String to = "recipient@example.com";
-	        String subject = "TestNG API Test Report";
-	        String body = "Please find the attached report.";
+	@AfterSuite
+	public void sendReportByEmail() throws Exception {
+		// Delay added just to be safe
+		Thread.sleep(3000);
 
+		String from = "cogsauto@gmail.com";
+		String to = "recipient@example.com";
+		String subject = "TestNG API Test Report";
+		String body = "Please find the attached report.";
 
-	    }
-
-	   
+	}
 
 }
