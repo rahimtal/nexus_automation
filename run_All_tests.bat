33c0b965b43c2a71fb481c0fd9342f1a6e5d27dd
talharahim <talharahim@gmail.com>
2026-04-14 16:44:04 +0500

    update

--- run_All_tests.bat
+++ run_All_tests.bat
@@ -1,4 +1,10 @@
 @echo off
 cd /d "c:\Users\Admin\Documents\GitHub\nexus_automation"
+mvn exec:java -Dexec.mainClass=com.NexustAPIAutomation.java.QuickDBRestore -DskipTests
+if %ERRORLEVEL% NEQ 0 (
+    echo Database restore failed! Aborting test execution.
+    pause
+    exit /b 1
+)
 mvn exec:java -Dexec.mainClass=com.NexustAPIAutomation.java.TestAndEmailExecutor -DskipTests
 pause
